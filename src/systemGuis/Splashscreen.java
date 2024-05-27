f595095acaae19ae8fbf9c902d7c6aec4134f202
coderpaco <bob.theerbes@gmail.com>
2024-05-26 21:27:41 -0400

    show list of 3 menu choices
    fixed menu choise (now uses the info popup instead of MenuChooser.java

--- src/systemGuis/Splashscreen.java
+++ src/systemGuis/Splashscreen.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
+import javax.swing.JOptionPane;
 
 //https://stackoverflow.com/questions/17665089/making-a-loading-screen-in-netbeans
 
@@ -34,6 +35,17 @@ public class Splashscreen {
 
         @Override
         protected void done() {
+            window.setVisible(false);
+            Object[] options = {"Sistema Vacio",
+                    "Sistema Anterior",
+                    "Solo Rubros"};
+            String infoMessage = "Seleccione un opcion: ";
+            String titleBar = "Proyecto por Davit y Diego";
+            //JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showOptionDialog(null,infoMessage,titleBar,JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,null,options,options[2]);
+        }
+            /*
             JFrame frame = menu;
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             //frame.setSize(390, 150);
@@ -41,7 +53,8 @@ public class Splashscreen {
             frame.setVisible(true);
             // Dispose window when background process is finished
             window.dispose();
-        }
+*/
+        
     }
 
     public static void main(String[] args) {
