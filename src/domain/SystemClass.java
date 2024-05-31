108a7129b0b4fcbd265314a3909bf7f1db299ba0
coderpaco <bob.theerbes@gmail.com>
2024-05-31 00:38:31 -0400

    SystemClass update
    this should be all the lists we'll need in here?

--- src/domain/SystemClass.java
+++ src/domain/SystemClass.java
@@ -21,13 +21,53 @@ public class SystemClass {
     
     // get/set the list with the info in the form, still working on this.
     public void setForemenList(String foremanName, String foremanId, String foremanAddress, int foremanYear){
-        Foreman foreman = new Foreman(foremanName, foremanId, foremanAddress, foremanYear);
-        System.out.println(foreman);
+        Foreman f = new Foreman(foremanName, foremanId, foremanAddress, foremanYear);
+        this.foreman.add(f);
+        System.out.println(f);
     }
     
     public ArrayList<Foreman> getForemenList(){
         return this.foreman;
     }    
     
+    public void setOwnersList(String ownerName, String ownerId, String ownerAddress, int ownerPhone){
+        Owner o = new Owner(ownerName, ownerId, ownerAddress, ownerPhone);
+        this.owners.add(o);
+        System.out.println(o);
+    }
     
+    public ArrayList<Owner> getOwnersList() {
+        return this.owners;
+    }
+    
+    public void setCategoriesList(String categoryName, String categoryDescription){
+        Category c = new Category(categoryName, categoryDescription);
+        this.categories.add(c);
+        System.out.println(c);
+    }
+        
+    public ArrayList<Category> getCategoriesList() {
+        return this.categories;
+    }
+    
+    public void setConstructionSitesList(Owner csOwner, Foreman csForeman, String csPermitNumber, String csAddress, int csStartMonth, int csStartYear, double csBudget) {
+        ConstructionSite cs = new ConstructionSite(csOwner, csForeman, csPermitNumber, csAddress, csStartMonth, csStartYear, csBudget);
+        this.constructionSites.add(cs);
+        System.out.println(cs);
+    }
+    
+    public ArrayList<ConstructionSite> getConstructionSitesList() {
+        return this.constructionSites;
+    }
+    
+    public void setExpendaturesList(ConstructionSite constructionSite, Category category, double amount, int month, int year, String description){
+        Expenditures e = new Expenditures(constructionSite, category, amount, month, year, description);
+        this.expenditures.add(e);
+        System.out.println(e);
+    }
+    
+    public ArrayList<Expenditures> getExpendituresList() {
+        return this.expenditures;
+    } 
+
 }
